388db7f17b5c0bbe731768bdd8254caf360f9d2b
HSLoveZL <hjmrezl@outlook.com>
2016-10-31 22:55:47 +0800

    Modify demo06 demos01

--- demo06/demos01.py
+++ demo06/demos01.py
@@ -1,2 +1,16 @@
 __author__ = 'HongSh'
 
+import sys
+
+def test():
+    args = sys.argv
+    if len(args == 1):
+        print 'Hello,World!'
+    elif len(args == 2):
+        print 'Hello, %s!' % args[1]
+    else:
+        print 'too many arguments!'
+
+if __name__ == '__main__':
+    test()
+
