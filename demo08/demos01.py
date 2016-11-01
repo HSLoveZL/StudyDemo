1265cfafdf753601d2f595984ce34c46f3904774
HSLoveZL <hjmrezl@outlook.com>
2016-11-01 10:58:59 +0800

    demos01.py

---
+++ demo08/demos01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+std1 = {'name':'HongSh', 'score':96}
+std2 = {'name':'Zhanglei', 'score':94}
+
+
+def print_score(std):
+    print '%s: %s' % (std['name'], std['score'])
+
+print_score(std1)
+print_score(std2)
+
