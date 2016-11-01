b3116855caeb14f7d5e9df13aa34fb71c1a2ff8e
HSLoveZL <hjmrezl@outlook.com>
2016-11-01 14:23:16 +0800

    Modify demos02.py

--- demo08/demos02.py
+++ demo08/demos02.py
@@ -10,8 +10,17 @@ class Student(object):
     def print_score(self):
         print '%s: %s' % (self.name, self.score)
 
+
+    def score_grade(self):
+        if (self.score >= 90):
+            print 'A'
+	elif (80 <= self.score < 90):
+	    print 'B'
+
 a = Student('HongSh', 98)
 b = Student('Zhanglei', 94)
 a.print_score()
 b.print_score()
+a.score_grade()
+b.score_grade()
 
